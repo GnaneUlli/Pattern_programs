c82d68497a09845412a0f10bddef4238e1984bf4
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-03 10:22:54 +0530

    Update Diamond_pattern.c

--- Diamond_pattern.c
+++ Diamond_pattern.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
 void printDiamond(int n) {
+    
     int i, j, space;
 
-    // Print upper half of the diamond
+    
     for (i = 1; i <= n; i++) {
-        // Print spaces before the star
+       
         for (space = 1; space <= n - i; space++) {
+            
             printf(" ");
         }
 
-        // Print stars
+        
         for (j = 1; j <= 2 * i - 1; j++) {
+            
             printf("*");
         }
 
         printf("\n");
     }
 
-    // Print lower half of the diamond
+    
     for (i = n - 1; i >= 1; i--) {
-        // Print spaces before the star
+      
         for (space = 1; space <= n - i; space++) {
+            
             printf(" ");
         }
 
-        // Print stars
+        
         for (j = 1; j <= 2 * i - 1; j++) {
+            
             printf("*");
         }
 
@@ -35,9 +40,11 @@ void printDiamond(int n) {
 }
 
 int main() {
+    
     int n;
 
-    printf("Enter the number of rows (odd): ");
+    printf("Enter the number of rows : ");
+    
     scanf("%d", &n);
 
     printDiamond(n);
